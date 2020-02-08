31c2ab019db1dbb565f4698ed30962ed5ecf2dbe
Ravi Teja <raviteja6820@gmail.com>
2020-02-08 18:15:58 +0530

    Linked List Insertion and Deletion

--- LinkedLists/LinkedList.java
+++ LinkedLists/LinkedList.java
@@ -1,15 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * LinkedList
  */
 public class LinkedList {
-    public static void main(String[] args) {
-        Node start = new Node(11);
-        start.next = new Node(22);
-        start.next.next = new Node(33);
-        start.next.next.next = new Node(44);
-        for(Node p = start; (p.next != null); p = p.next){
+    Node start;
+    public void insertion(int data){
+        Node p = start;
+        if(p != null){
+            while(p.next != null){
+                if(p.data >= data) break;
+                p = p.next;
+            }
+            if(p.next == null && p.data < data) p.next = new Node(data);
+            else {
+                Node q = new Node(p.data);
+                q.next = p.next;
+                p.data = data;
+                p.next = q;
+            }
+        } else {
+            start = new Node(data);
+        }
+    }
+    public void delete(int data){
+        Node p = start;
+        if(p != null)
+        {
+            while(p.next != null){
+            if(p.next.data == data){
+                p.next = p.next.next;
+                break;
+            }
+            if(p.data == data) start = p.next;
+            p = p.next;
+            }
+            if(p.data == data) start = null;
+        }
+    }
+    public void display(){
+        Node p = start;
+        if(p != null)
+        {
+            while(p.next != null){
             System.out.println(p.data);
+            p = p.next;
+            }
+            System.out.println(p.data);
+        }
+    }
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        System.out.println("Insert new element in linked list :");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        LinkedList ll = new LinkedList();
+        boolean exit = false;
+        while(!exit)
+        {
+            System.out.println("Enter Option :\n 1.Insert \n 2.Delete \n 3.Display");
+            int option = Integer.parseInt(br.readLine());
+            switch(option){
+                case 1: int data = Integer.parseInt(br.readLine()); ll.insertion(data); break;
+                case 2: int del = Integer.parseInt(br.readLine()); ll.delete(del); break;
+                case 3: ll.display(); break;
+                default: exit = true;
+            }
         }
     }
 }
--- LinkedLists/Node.java
+++ LinkedLists/Node.java
@@ -7,6 +7,10 @@ public class Node {
     Node(int data){
         this.data = data;
     }
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
     public static void main(String[] args) {
         
     }
